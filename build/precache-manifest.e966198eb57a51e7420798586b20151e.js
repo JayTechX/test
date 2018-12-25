860a9269f9bc83162b8464c5de4631c15df22469
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-12-25 22:38:55 +0100

    first commit

---
+++ build/precache-manifest.e966198eb57a51e7420798586b20151e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "5348a0f21c43273667db6977ea81d00c",
+    "url": "/static/media/exit.5348a0f2.svg"
+  },
+  {
+    "revision": "0d6b74bfd7913dc048cf",
+    "url": "/static/css/main.6d3da604.chunk.css"
+  },
+  {
+    "revision": "eff6edb176ca0eaac3cbdcc6c1c95d1d",
+    "url": "/static/media/next.eff6edb1.svg"
+  },
+  {
+    "revision": "7ca47f74379a632dd50a",
+    "url": "/static/js/1.7ca47f74.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "023586736eb3b67e3b879caae5deb47b",
+    "url": "/static/media/playlist.02358673.svg"
+  },
+  {
+    "revision": "046923e651489f6a493d7cbb7c2d8401",
+    "url": "/static/media/play.046923e6.svg"
+  },
+  {
+    "revision": "d5087dca355f369c9271507b20a4ef5f",
+    "url": "/static/media/enter.d5087dca.svg"
+  },
+  {
+    "revision": "8bdf2406276e0c2f84e008d065c4e991",
+    "url": "/static/media/notes.8bdf2406.svg"
+  },
+  {
+    "revision": "0d6b74bfd7913dc048cf",
+    "url": "/static/js/main.0d6b74bf.chunk.js"
+  },
+  {
+    "revision": "2a2ea2f3d6a07c23d5640f4d75d43319",
+    "url": "/static/media/user.2a2ea2f3.svg"
+  },
+  {
+    "revision": "18bae7b0c1554274bdefb9d9fbb0ab9f",
+    "url": "/static/media/karaoke.18bae7b0.svg"
+  },
+  {
+    "revision": "35ea1adf1eb7444becea5e29da91ff78",
+    "url": "/static/media/options_two.35ea1adf.svg"
+  },
+  {
+    "revision": "e85859270eabf1d0e99b6f2b39fb7569",
+    "url": "/static/media/previous.e8585927.svg"
+  },
+  {
+    "revision": "2a63962de44688ac01f6fda1d1a4c921",
+    "url": "/static/media/playbutton.2a63962d.svg"
+  },
+  {
+    "revision": "bfb53c3efbef03c9d19edb40e6575084",
+    "url": "/static/media/pause.bfb53c3e.svg"
+  },
+  {
+    "revision": "7ca47f74379a632dd50a",
+    "url": "/static/css/1.a2db2ddc.chunk.css"
+  },
+  {
+    "revision": "8a2af7a48b5ec8a15e8a137d6ad12320",
+    "url": "/index.html"
+  }
+];
